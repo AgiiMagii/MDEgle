7cf6969077afb97e64eec56589193cb30c8be180
Agija <arajaagija@gmail.com>
2026-04-18 11:01:46 +0300

    Enhance tree drawing with color selection and refactor
    Refactor Program.cs to allow user selection of tree, trunk, ground, and background colors from a predefined list. Add helper methods for color input and colored output. Simplify size input and remove unused array logic. Add new tree image files and update build artifacts. Improve overall user experience and code clarity.

--- MDEgle/Program.cs
+++ MDEgle/Program.cs
@@ -1,61 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace MDEgle
 {
     internal class Program
     {
         static int width;
-        static int heigh;
+        static int height;
+        static string crownColor = "Green";
+        static string trunkColor = "Gray";
+        static string groundColor = "Green";
+        static string backgroundColor = "White";
+        static List<string> colors = new List<string> { "Black", "Blue", "Cyan", "DarkBlue", "DarkCyan", "DarkGray", "DarkGreen", "DarkMagenta", "DarkRed", "DarkYellow", "Gray", "Green", "Magenta", "Red", "White", "Yellow" };
         static void Main(string[] args)
         {
-            width = GetUserInput(" Set the picture width!", " (choose an integer 7 or above)");
-            heigh = GetUserInput(" Set the picture heigh!", " (choose an integer 7 or above)");
+            height = GetUserInput(" Set the picture size!", " (choose an integer 7 or above)");
+            width = height + 1;
+            Console.WriteLine(string.Join(", ", colors));
+            crownColor = GetColorInput(" Set the crown color! (choose from the list below)", crownColor);
+            trunkColor = GetColorInput(" Set the trunk color! (choose from the list below)", trunkColor);
+            groundColor = GetColorInput(" Set the ground color! (choose from the list below)", groundColor);
+            backgroundColor = GetColorInput(" Set the background color! (choose from the list below)", backgroundColor);
             Console.Clear();
-            char[,] Patterns = new char[heigh, width];
-            for (int i = 0; i < heigh; i++)
-            {
-                for (int j = 0; j < width; j++)
-                {
-                    Patterns[i, j] = '.';
-                }
-            }
+            DrawTree();
+            Console.ReadLine();
+        }
+        static void DrawTree()
+        {
             int tree = width / 2;
-            for (int i = 0; i < heigh; i++)
+
+            for (int i = 0; i < height; i++)
             {
                 for (int j = 0; j < width; j++)
                 {
-                    if (j >= tree - i && j <= tree + i && i < heigh / 2)
+                    char symbol = '.';
+                    string color = backgroundColor;
+
+                    if (j >= tree - i && j <= tree + i && i < height / 2)
                     {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write('^' + " ");
-                        Console.ResetColor();
+                        symbol = '^';
+                        color = crownColor;
                     }
-                    else if (j >= tree - 1 && j <= tree + 1 && i >= heigh / 2 && i < heigh - 3)
+                    else if (j >= tree - 1 && j <= tree + 1 && i >= height / 2 && i < height - 3)
                     {
-                        Console.ForegroundColor = ConsoleColor.Gray;
-                        Console.Write('|' + " ");
-                        Console.ResetColor();
+                        symbol = '|';
+                        color = trunkColor;
                     }
-                    else if (i >= heigh - 3)
+                    else if (i >= height - 3)
                     {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write('^' + " ");
-                        Console.ResetColor();
+                        symbol = '^';
+                        color = groundColor;
                     }
-                    else Console.Write($"{Patterns[i, j]} ");
+
+                    PrintWithColor(symbol, color);
+                    Console.Write(" ");
                 }
                 Console.WriteLine();
             }
-            Console.ReadLine();
         }
-        static int GetUserInput(string message1, string message2)
+        static int GetUserInput(string message1, string message2) //Get user input for the size of the picture, validate it and return the value, if the input is invalid, ask the user to try again
         {
             int value;
             bool result;
             do
             {
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine(message1);
-                Console.ResetColor();
+                PrintWithColor($"{message1}\n", "Cyan");
                 Console.WriteLine(message2);
                 string input = Console.ReadLine();
                 result = int.TryParse(input, out value);
@@ -66,6 +74,41 @@ namespace MDEgle
             }
             while (!result || value < 7);
             return value;
+        }
+        static string GetColorInput(string message, string defaultColor) //Get color input from user, validate it and return the color, if the user presses Enter, return the default color
+        {
+            string color;
+            bool isValidColor;
+
+            do
+            {
+                PrintWithColor($"{message} (press Enter for default: {defaultColor})\n", "Cyan");
+                color = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(color))
+                    return defaultColor;
+
+                isValidColor = colors.Contains(color);
+
+                if (!isValidColor)
+                {
+                    Console.WriteLine("False input! Try again!");
+                }
+            }
+            while (!isValidColor);
+
+            return color;
+        }
+        static void PrintWithColor(object value, string color = "White") //Print text in specified color, default is white
+        {
+            if (!Enum.TryParse(color, true, out ConsoleColor consoleColor))
+            {
+                consoleColor = ConsoleColor.White;
+            }
+            Console.ForegroundColor = consoleColor;
+
+            Console.Write(value);
+            Console.ResetColor();
         }
     }
 }
